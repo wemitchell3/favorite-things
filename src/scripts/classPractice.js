7a73f91e2782ea93ba44966ff933f98a33c6578a
William Mitchell <wemitchell3@gmail.com>
2019-03-28 15:12:48 -0500

    Uploading address book code to start favorite things practice.

---
+++ src/scripts/classPractice.js
@@ -0,0 +1,23 @@
+const container = document.querySelector("#addressList")
+
+document.querySelector("#saveEntry").addEventListener("click", event => {
+    /*
+        Collect the user put by selecting the input fields, one
+        at a time, and accessing the `value` property
+    */
+    const personName = document.querySelector("#fullName").value
+    const personAddress = document.querySelector("#address").value
+
+ 
+    console.log("Person Name", personName.value)
+    console.log("Person Address", personAddress.value)
+    // Once you have collected all the values, update the DOM
+    // with some HTML
+    container.innerHTML += `
+        <section>
+            <h1>${personName}</h1>
+            <div>${personAddress}</div>
+        </section>
+    `
+
+})
